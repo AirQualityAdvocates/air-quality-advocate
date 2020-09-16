4617a805602205240249fd09c5a6c6622350a3a4
Greg Mall <gregmall157@gmail.com>
2020-09-16 15:12:51 -0700

    wote queue file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,24 @@ const app = require('../lib/app');
 
 describe('air-quality-advocate routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+  it('has a POST that inserts lat/lon, phone number, range and air quality into a notifications table', async() => {
+    const response = await request(app)
+      .post('/api/v1/airquality')
+      .send({
+        phone: '503-555-1212',
+        coordinates: [
+          -76.84619,
+          39.143196
+        ],
+        range: '20 miles',
+        threshold: 35
+
+
+      });
+
+
+  });
+
 });
