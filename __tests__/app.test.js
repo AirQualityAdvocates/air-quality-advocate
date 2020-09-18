1f108060f2e0c07158a98760ddd3e7f37b6d9a50
Greg Mall <gregmall157@gmail.com>
2020-09-18 14:19:50 -0700

    wrote a get route and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Notification = require('../lib/model/notification');
 
 describe('air-quality-advocate routes', () => {
   beforeEach(() => {
@@ -23,6 +24,7 @@ describe('air-quality-advocate routes', () => {
       
 
     expect(response.body).toEqual({
+
       id: expect.any(String),
       phone: '503-555-1212',
       lon: -122,
@@ -34,5 +36,41 @@ describe('air-quality-advocate routes', () => {
 
 
   });
+  it('has a GET that gets user information from the notification table',
+    async() => {
+
+      const searchedUser = await Notification.insert({
+        phone: '503-555-1212',
+        lon: -122,
+        lat: 45,
+       
+        range: '20 miles',
+        threshold: 35
+
+      });
+      
+      const response = await request(app)
+        .get('/api/v1/notifications');
+      
+
+
+      
+  
+
+      expect(response.body).toEqual([{
+
+        id: expect.any(String),
+        phone: '503-555-1212',
+        lon: -122,
+        lat: 45,
+        range: '20 miles',
+        threshold: 35
+
+      }]);
+      
+
+
+    });
+
 
 });
